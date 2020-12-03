f67b08240ce4d615589d84643848d891dcf76713
Yifei <valkriaine@outlook.com>
2020-12-03 06:42:56 -0800

    fixed an issue when geo fences are not restored after device reboots

--- app/src/main/java/com/digitalsmart/mutify/broadcast_receivers/RebootBroadcastReceiver.java
+++ app/src/main/java/com/digitalsmart/mutify/broadcast_receivers/RebootBroadcastReceiver.java
@@ -1,19 +1,10 @@
 package com.digitalsmart.mutify.broadcast_receivers;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
-import androidx.core.app.NotificationCompat;
-import com.digitalsmart.mutify.R;
 import com.digitalsmart.mutify.UserDataManager;
-
-import static com.digitalsmart.mutify.util.Constants.NOTIFICATION_ID;
-import static com.digitalsmart.mutify.util.Constants.PACKAGE_NAME;
 
 //restore geofencing after the device reboots
 public class RebootBroadcastReceiver extends BroadcastReceiver
